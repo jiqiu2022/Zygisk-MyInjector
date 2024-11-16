933b962d6e2c97fc0cbdd2d8dac9ef99061dda4b
jiqiu2021 <jiqiu2021@gmail.com>
2024-11-16 23:04:25 +0800

    fix errlog

--- module/src/main/cpp/log.h
+++ module/src/main/cpp/log.h
@@ -12,5 +12,5 @@
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-
+#define PLOGE(fmt, args...) LOGE(fmt " failed with %d: %s", ##args, errno, strerror(errno))
 #endif //ZYGISK_IL2CPPDUMPER_LOG_H
